7fc96ccee92b8df978dbaf527923a18c1dd485d2
Banixx <banidoesch@gmail.com>
2024-03-07 22:03:25 +0100

    First Ligth, Materials

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ import { nodeTemplate, edgeTemplate } from './elements.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Erstelle ein planares Licht
+const light = new THREE.DirectionalLight(0xffffff, 1); // Farbe, Intensität
+light.position.set(1, 1, 1); // Setze die Richtung des Lichts
+scene.add(light);
+
 // Positioniere die Kamera
 camera.position.z = 6;
+camera.position.y = 5;
+camera.position.x = -1;
 
 // Erstelle Nodes
 const node1 = nodeTemplate(scene, 0, 0, 0);
@@ -20,9 +27,10 @@ const node2 = nodeTemplate(scene, 2, 2, 0);
 // Verbinde Nodes mit einer Edge
 const edge = edgeTemplate(scene, node1, node2);
 // Füge GridHelper hinzu
-const size = 10;
+const size = 30;
 const divisions = 10;
-const gridHelper = new THREE.GridHelper(size, divisions);
+const gridHelper = new THREE.GridHelper(30, 10, new THREE.Color(0x2d2020), new THREE.Color(0x4d6060));
+gridHelper
 scene.add(gridHelper);
 
 // Animationsloop
--- elements.js
+++ elements.js
@@ -2,7 +2,7 @@ console.log("elements.js geladen")
 import * as THREE from 'three';
 export const nodeTemplate = (scene, x, y, z) => {
     const geometry = new THREE.SphereGeometry(0.5, 32, 32);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const material = new THREE.MeshLambertMaterial({ color: 0x3030b0 });
     const sphere = new THREE.Mesh(geometry, material);
     sphere.position.set(x, y, z);
     scene.add(sphere);
@@ -10,7 +10,14 @@ export const nodeTemplate = (scene, x, y, z) => {
 };
 
 export const edgeTemplate = (scene, start, end) => {
-    const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    const material = new THREE.LineDashedMaterial( {
+        color: 0xdfefff,
+        linewidth: 1,
+        scale: 1,
+        dashSize: 3,
+        gapSize: 1,
+        isLineDashedMaterial: true
+    } );
     const points = [];
     points.push(start.position);
     points.push(end.position);
