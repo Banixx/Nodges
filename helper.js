9e18505f5cb158bcbdf473cb5f03f13538a262bc
Banixx <banidoesch@gmail.com>
2024-03-07 20:29:21 +0100

    Initial

---
+++ helper.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three';
+
+// Funktion, die das Gitternetz zur Szene hinzufügt
+export function addGridHelpers(scene) {
+    // Farben für die GridHelpers
+    const c1 = new THREE.Color(0x125634 ); // Bordeaux für Mittellinien
+    const c2 = new THREE.Color(0x561234); // Ein passendes Blau für Gitterlinien
+    const c3 = new THREE.Color(0x563412); // Ein dezentes Grün für Gitterlinien
+
+    // Erstelle das Gitternetz in der XZ-Ebene (Boden)
+    const gridHelperXZ = new THREE.GridHelper(10, 10, c1, c1);
+    scene.add(gridHelperXZ);
+
+    // Erstelle das Gitternetz in der XY-Ebene (Wand/Front)
+    const gridHelperXY = new THREE.GridHelper(10, 10, c2, c2);
+    gridHelperXY.rotation.x = Math.PI / 2;
+    scene.add(gridHelperXY);
+
+    // Erstelle das Gitternetz in der YZ-Ebene (Seitenwand)
+    const gridHelperYZ = new THREE.GridHelper(10, 10, c3, c3);
+    gridHelperYZ.rotation.z = Math.PI / 2;
+    scene.add(gridHelperYZ);
+}
