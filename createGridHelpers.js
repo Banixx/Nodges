799b0bfadd9e575d127f979d54fd84cc2c67f9e8
Banixx <banidoesch@gmail.com>
2024-03-18 12:03:46 +0100

    Integration createGridhelper

--- createGridHelpers.js
+++ createGridHelpers.js
@@ -2,9 +2,9 @@
 import * as THREE from 'three';
 
 function createGridHelpers(scene) {
-
+console.log("createGridhelpers funktion ausgeführt")
     const helperLayer = new THREE.Layers();
-    helperLayer.set(1);  
+    helperLayer.enable(0,1);  
 
     const size = 10;
     const divisions = 10;
@@ -16,38 +16,33 @@ function createGridHelpers(scene) {
     const gridHelperXY = new THREE.GridHelper(size, divisions,gridColorXY,gridColorXY);
     gridHelperXY.name = "XY"
     
-    gridHelperXY.layers.enable(2); // Aktiviere diesen Layer für den GridHelper
-    gridHelperXY.layers.disable(0); // Aktiviere diesen Layer für den GridHelper
+    //gridHelperXY.layers.enable(2); 
+    //gridHelperXY.layers.disable(0); 
     scene.add(gridHelperXY);
 
     gridHelperXY.rotation.x = Math.PI / 2
     gridHelperXY.position.z = -5;
     gridHelperXY.position.y = 5;
 
-
     // YZ-Ebene
     const gridHelperYZ = new THREE.GridHelper(size, divisions,gridColorYZ,gridColorYZ);
     gridHelperYZ.name = "YZ"
 
-    gridHelperYZ.layers.enable(3);
-    gridHelperYZ.layers.disable(0); 
-
+    //gridHelperYZ.layers.enable(3);
+    //gridHelperYZ.layers.disable(0); 
 
     gridHelperYZ.rotation.z = Math.PI / 2;
     gridHelperYZ.position.x = -size / 2;
     gridHelperYZ.position.y = +5
-  
-
-    
 
     scene.add(gridHelperYZ);
 
     // XZ-Ebene (standardmäßig von Three.js bereitgestellt)
     const gridHelperXZ = new THREE.GridHelper(size, divisions,gridColorXZ,gridColorXZ);
     
-    gridHelperXZ.layers.enable(4);
-    gridHelperXZ.layers.enable(2);
-    gridHelperXZ.layers.disable(0); 
+    //gridHelperXZ.layers.enable(4);
+    //gridHelperXZ.layers.enable(2);
+    //gridHelperXZ.layers.disable(0); 
 
 
     gridHelperXZ.name = "XZ"
